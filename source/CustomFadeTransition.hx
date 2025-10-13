026b4acb4d042b60a4eb6f7ad32424500c3682d0
ようば/Youba <146600129+youbadao-sussy@users.noreply.github.com>
2025-10-13 09:18:11 +0900

    SplitVocal in CustomFadeTransition.hx

--- source/CustomFadeTransition.hx
+++ source/CustomFadeTransition.hx
@@ -22,34 +22,53 @@ class CustomFadeTransition extends MusicBeatSubstate {
 	var transBlack:FlxSprite;
 	var transGradient:FlxSprite;
 
-	public function new(duration:Float, isTransIn:Bool) {
-		super();
-
+	//public function new(duration:Float, isTransIn:Bool) {
+	var duration:Float;
+	public function new(duration:Float, isTransIn:Bool)
+	{
+		this.duration = duration;
 		this.isTransIn = isTransIn;
+		super();
+	}
+
+		/*this.isTransIn = isTransIn;
 		var zoom:Float = CoolUtil.boundTo(FlxG.camera.zoom, 0.05, 1);
 		var width:Int = Std.int(FlxG.width / zoom);
-		var height:Int = Std.int(FlxG.height / zoom);
+		var height:Int = Std.int(FlxG.height / zoom);*/
+	override function create()
+	{
+		cameras = [FlxG.cameras.list[FlxG.cameras.list.length-1]];
+		var width:Int = Std.int(FlxG.width / Math.max(camera.zoom, 0.001));
+		var height:Int = Std.int(FlxG.height / Math.max(camera.zoom, 0.001));
 		transGradient = FlxGradient.createGradientFlxSprite(width, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
 		transGradient.scrollFactor.set();
+		transGradient.screenCenter(X);
 		add(transGradient);
 
-		transBlack = new FlxSprite().makeGraphic(width, height + 400, FlxColor.BLACK);
+		/*transBlack = new FlxSprite().makeGraphic(width, height + 400, FlxColor.BLACK);
+		transBlack.scrollFactor.set();*/
+		transBlack = new FlxSprite().makeGraphic(1, 1, FlxColor.BLACK);
+		transBlack.scale.set(width, height + 400);
+		transBlack.updateHitbox();
 		transBlack.scrollFactor.set();
+		transBlack.screenCenter(X);
 		add(transBlack);
 
-		transGradient.x -= (width - FlxG.width) / 2;
+		/*transGradient.x -= (width - FlxG.width) / 2;
 		transBlack.x = transGradient.x;
 
-		if(isTransIn) {
-			transGradient.y = transBlack.y - transBlack.height;
+		if(isTransIn) {*/
+		if(isTransIn)
+			/*transGradient.y = transBlack.y - transBlack.height;
 			FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
 				onComplete: function(twn:FlxTween) {
 					close();
 				},
 			ease: FlxEase.linear});
-		} else {
+		} else {*/
+		else
 			transGradient.y = -transGradient.height;
-			transBlack.y = transGradient.y - transBlack.height + 50;
+			/*transBlack.y = transGradient.y - transBlack.height + 50;
 			leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
 				onComplete: function(twn:FlxTween) {
 					if(finishCallback != null) {
@@ -63,28 +82,45 @@ class CustomFadeTransition extends MusicBeatSubstate {
 			transBlack.cameras = [nextCamera];
 			transGradient.cameras = [nextCamera];
 		}
-		nextCamera = null;
+		nextCamera = null;*/
+		super.create();
 	}
 
 	override function update(elapsed:Float) {
-		if(isTransIn) {
+		/*if(isTransIn) {
 			transBlack.y = transGradient.y + transGradient.height;
 		} else {
 			transBlack.y = transGradient.y - transBlack.height;
-		}
+		}*/
 		super.update(elapsed);
-		if(isTransIn) {
-			transBlack.y = transGradient.y + transGradient.height;
-		} else {
-			transBlack.y = transGradient.y - transBlack.height;
-		}
-	}
+		//if(isTransIn) {
+		
+		final height:Float = FlxG.height * Math.max(camera.zoom, 0.001);
+		final targetPos:Float = transGradient.height + 50 * Math.max(camera.zoom, 0.001);
+		if(duration > 0)
+			transGradient.y += (height + targetPos) * elapsed / duration;
+		else
+			transGradient.y = (targetPos) * elapsed;
 
-	override function destroy() {
+		if(isTransIn)
+			transBlack.y = transGradient.y + transGradient.height;
+		//} else {
+		else
+			transBlack.y = transGradient.y - transBlack.height;
+		//}
+	//}
+
+	/*override function destroy() {
 		if(leTween != null) {
 			finishCallback();
-			leTween.cancel();
+			leTween.cancel();*/
+		
+		if(transGradient.y >= targetPos)
+		{
+			close();
+			if(finishCallback != null) finishCallback();
+			finishCallback = null;
 		}
-		super.destroy();
+		//super.destroy();
 	}
 }
