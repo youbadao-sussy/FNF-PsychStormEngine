21d1ed436901c60144372a88aa9b309e4cc24209
youbadao-sussy <youbadao@icloud.com>
2025-11-02 13:00:48 +0900

    a

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -87,9 +87,9 @@ class PauseSubState extends MusicBeatSubstate
 		
 		var levelComposer:FlxText = new FlxText(20, 15 + 32, 0, "", 32);
 		levelComposer.text += "Artist: " + comTxt;
-		if(PlayState.SONG.song == 'bpeebo' ) comTxt = 'Kawai Sprite';
-		if(PlayState.SONG.song == 'fresh') comTxt = 'Kawai Sprite';
-		if(PlayState.SONG.song == 'dad-battle' ) comTxt = 'Kawai Sprite';
+		if(PlayState.SONG.song == 'Bopeebo' ) comTxt = 'Kawai Sprite';
+		if(PlayState.SONG.song == 'Fresh') comTxt = 'Kawai Sprite';
+		if(PlayState.SONG.song == 'Dad Battle' ) comTxt = 'Kawai Sprite';
 		if(PlayState.SONG.song == null ) comTxt = 'None';
 		levelComposer.scrollFactor.set();
 		levelComposer.setFormat(Paths.font("vcr.ttf"), 32);
@@ -103,14 +103,14 @@ class PauseSubState extends MusicBeatSubstate
 		levelDifficulty.updateHitbox();
 		add(levelDifficulty);
 
-		var blueballedTxt:FlxText = new FlxText(20, 15 + 86, "", 32);
+		var blueballedTxt:FlxText = new FlxText(20, 15 + 96, "", 32);
 		blueballedTxt.text = "Blueballed: " + PlayState.deathCounter;
 		blueballedTxt.scrollFactor.set();
 		blueballedTxt.setFormat(Paths.font('vcr.ttf'), 32);
 		blueballedTxt.updateHitbox();
 		add(blueballedTxt);
 
-		practiceText = new FlxText(20, 15 + 101, 0, "PRACTICE MODE", 32);
+		practiceText = new FlxText(20, 15 + 121, 0, "PRACTICE MODE", 32);
 		practiceText.scrollFactor.set();
 		practiceText.setFormat(Paths.font('vcr.ttf'), 32);
 		practiceText.x = FlxG.width - (practiceText.width + 20);
@@ -118,7 +118,7 @@ class PauseSubState extends MusicBeatSubstate
 		practiceText.visible = PlayState.instance.practiceMode;
 		add(practiceText);
 
-		var chartingText:FlxText = new FlxText(20, 15 + 101, 0, "CHARTING MODE", 32);
+		var chartingText:FlxText = new FlxText(20, 15 + 121, 0, "CHARTING MODE", 32);
 		chartingText.scrollFactor.set();
 		chartingText.setFormat(Paths.font('vcr.ttf'), 32);
 		chartingText.x = FlxG.width - (chartingText.width + 20);
