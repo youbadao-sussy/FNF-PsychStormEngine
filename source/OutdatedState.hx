4edadf723aa96203ce3573960bcafd926c75e5d0
youbadao-sussy <youbadao@icloud.com>
2025-11-02 13:44:19 +0900

    ah

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -41,7 +41,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
+				CoolUtil.browserLoad("https://github.com/youbadao-sussy/FNF-PsychStormEngine/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
