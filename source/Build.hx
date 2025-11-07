02b19f56d09d62cdd67fa2096130448cd5600cae
ようば/Youba <146600129+youbadao-sussy@users.noreply.github.com>
2025-11-07 16:31:39 +0900

    Introduce BuildInfo class for build number handling
    Add BuildInfo class to manage build number with macro support.

--- source/Build.hx
+++ source/Build.hx
@@ -1,6 +1,20 @@
 package;
 
-class Build 
-{
-	public static var number:Int = 10;
+import haxe.macro.Context;
+import haxe.macro.Expr;
+
+class BuildInfo {
+	public static var BUILD_NUMBER:String;
+
+
+	public static function init() {
+		#if macro
+			var buildNumber = Context.definedValue("build_number");
+			if (buildNumber == null) {
+				buildNumber = "N/A"; // デフォルト値
+			}
+			// コンパイル時に値を埋め込む
+			Context.defineVar("BuildInfo.BUILD_NUMBER", haxe.macro.Expr.fromConstant(Constant.String(buildNumber)))
+		#end
+	}
 }
