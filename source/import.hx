96e428c8dc2525ecbf66ac14e7b4408553a01a9e
ようば/Youba <146600129+youbadao-sussy@users.noreply.github.com>
2025-11-15 17:16:30 +0900

    import7

--- source/import.hx
+++ source/import.hx
@@ -2,11 +2,18 @@ import Paths;
 
 import backend.ui.*; //Psych-UI
 
-
+//Flixel
+import flixel.sound.FlxSound;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
+import flixel.math.FlxMath;
 import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.addons.transition.FlxTransitionableState;
