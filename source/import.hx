cc969000508ea3173bce9f2792b40c19430603b5
ようば/Youba <146600129+youbadao-sussy@users.noreply.github.com>
2025-11-15 17:11:21 +0900

    import5

--- source/import.hx
+++ source/import.hx
@@ -5,5 +5,7 @@ import backend.ui.*; //Psych-UI
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
+import flixel.text.FlxText;
 import flixel.group.FlxSpriteGroup;
