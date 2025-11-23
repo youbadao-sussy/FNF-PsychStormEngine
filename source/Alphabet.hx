c50d48e6d5260f44bc1445c93604c43d6d5a38b6
ようば/Youba <146600129+youbadao-sussy@users.noreply.github.com>
2025-11-23 15:51:16 +0900

    FlxSound

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -9,7 +9,7 @@ import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxMath;
 import flixel.math.FlxPoint;
 import flixel.util.FlxTimer;
-import flixel.system.FlxSound;
+import flixel.sound.FlxSound;
 import flash.media.Sound;
 import flixel.util.FlxColor;
 
